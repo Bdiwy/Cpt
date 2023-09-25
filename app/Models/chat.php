ccefd96f28459603e3512ea64868af45f423cbab
Ahmed Bdiwy <ahmedbedawi22@gmail.com>
2023-09-25 10:58:18 +0300

    updates

--- app/Models/chat.php
+++ app/Models/chat.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class chat extends Model
 {
     use HasFactory;
+    protected $table = 'chats';
+
+
+    protected $fillable=['user_id','situation','created_at','updated_at'];
+
+
+    protected $hiden=['created_at','updated_at'];
+    
+    
 }
